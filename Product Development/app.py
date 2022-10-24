a9dbfba04e3825e82be4edba625cf73ae982eb87
snare0625 <86784170+snare0625@users.noreply.github.com>
2022-10-24 03:57:53 +0200

    API call 1st attempt

--- Product Development/app.py
+++ Product Development/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, session, request, redirect
 from flask_socketio import SocketIO, send
-#from twilio.rest import Client
+from twilio.rest import Client
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123'
@@ -34,6 +34,20 @@ def help():
 @app.route("/chat")
 def chat():
     return render_template("chat.html")
+
+@app.route("/calling")
+def callclick():
+    account_sid = 'AC53c02b716c92cd490d905c2cb1367a7a'
+    auth_token = '460241dcc2bcbb0b3899cf117600c37a'
+    client = Client(account_sid, auth_token)
+
+    call = client.calls.create(
+                            url='https://rackley-hummingbird-7239.twil.io/assets/Recording.m4a',
+                            to='+27825748542',
+                            from_='+14782428486'
+                        )
+
+    print(call.sid)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
